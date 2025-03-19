871e1571934caca3f878f7057e95383f1414b85a
sakibalt <sakibrohan99@gmail.com>
2025-03-19 14:08:20 +0600

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,18 +1,7 @@
-import { config } from "dotenv";
 import { cors } from "hono/cors";
 
-config();
-
-const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
-  ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:4000", "https://mugiwaraanime.great-site.net", "*"];
-
-const corsConfig = cors({
-  allowMethods: ["GET", "POST", "OPTIONS"],
-  allowHeaders: ["Content-Type", "Authorization"],
-  maxAge: 600,
-  credentials: true,
-  origin: allowedOrigins,
+export default cors({
+  origin: ["https://mugiwaraanime.great-site.net"], // Allow frontend access
+  allowMethods: ["GET", "POST"], // Allow GET & POST requests
+  allowHeaders: ["Content-Type"], // Allow content type headers
 });
-
-export default corsConfig;
