ebc26231714e891cc2210e4839e14ac0c5b908aa
sakibalt <sakibrohan99@gmail.com>
2025-03-20 16:40:52 +0600

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,12 @@
 import { cors } from "hono/cors";
 
 export default cors({
-  origin: ["https://mugiwaraanime.great-site.net"], // Allow frontend access
+  const allowedOrigins = [
+  "http://localhost:4000",
+  "http://mugiwaraanime.great-site.net",
+  "https://mugiwaraanime.great-site.net",
+  "*"
+];, // Allow frontend access
   allowMethods: ["GET", "POST"], // Allow GET & POST requests
   allowHeaders: ["Content-Type"], // Allow content type headers
 });
